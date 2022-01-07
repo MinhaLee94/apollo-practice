c864363e8976528ebcabad577f9d76f1d210209c
MinhaLee94 <shjune1994@gmail.com>
2022-01-07 01:27:24 -0500

    Build home and detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,29 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
 
-export default Detail => <h1>Detail</h1>;
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+const Detail = () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id } // convert String id to int
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
+
+export default Detail;
