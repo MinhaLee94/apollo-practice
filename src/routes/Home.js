20278839fcee363c882254c34abce783332dde0a
MinhaLee94 <shjune1994@gmail.com>
2022-01-07 23:48:12 -0500

    CSS works

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,38 +13,47 @@ const GET_MOVIES = gql`
 `;
 
 const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 `;
 
 const Header = styled.header`
-	background-image: linear-gradient(-45deg, #d754ab, #fd723a);
-	height: 45vh;
-	color: white;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 `;
 
 const Title = styled.h1`
-	font-size: 60px;
-	font-weight: 600;
-	margin-bottom: 20px;
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
 `;
 
 const Subtitle = styled.h3`
-  	font-size: 35px;
+  font-size: 35px;
 `;
 
 const Loading = styled.div`
-	font-size: 18px;
-	opacity: 0.5;
-	font-weight: 500;
-	margin-top: 10px;
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
 `;
 
 
@@ -57,8 +66,13 @@ const Home = () => {
 				<Subtitle>React, GraphQL and Apollo</Subtitle>
 			</Header>
 			{loading && <Loading>Loading...</Loading>}
-			{!loading && data.movies && data.movies.map((movie) => 
-				<Movie key={movie.id} id={movie.id} />)}
+			{!loading && data.movies && (
+				<Movies>
+					{data.movies.map((movie) => 
+						<Movie key={movie.id} id={movie.id} bg={movie.medium_cover_image}/>
+					)})
+				</Movies>
+			)}
 		</Container>
 	);
 };
